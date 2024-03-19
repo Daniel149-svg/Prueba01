ab011a307cdae1c5527a32dfc95b983a4c87c978
daniel trujillo <oscartrujillo159@gmail.com>
2024-03-19 13:03:36 -0600

    Esta es la clase 9 de diseno web

---
+++ primer-proyecto/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#btn_enviar").click(()=>{
+        guardar_json();
+    })
+});
+
+function guardar_json(){
+    let mi_json = {
+        nombre: $("#nombre").val(),
+        apellido: $("#apellido").val(),
+        fecha: $("#fecha").val(),
+        profesion: $("#profesion").val(),
+
+        
+    }
+    console.log("Hola mi nombre es " + mi_json.nombre + " " + mi_json.apellido +"\n" +
+    "Mi fecha de nacimiento es " + mi_json.fecha + " y mi profecion es " + mi_json.profesion);
+
+
+    mi_json.saludo = prompt("Saluda de manera unica");
+    console.log(mi_json.saludo)
+
+    delete mi_json["saludo"];
+    console.log(mi_json);
+};
